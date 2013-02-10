5d90e982b8ebdebcea94eb1b13e2d97eda4ec876
ZheLih <lykhovyd@gmail.com>
2013-02-10 15:37:38 +0200

    HelloSwing

---
+++ src/HelloSwing.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class HelloSwing {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("HelloSwing");
+		final JLabel label = new JLabel("Hello Swing");
+		frame.getContentPane().add(label); //different from AWT
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack(); //make compact size
+		frame.setVisible(true);
+	}
+}
