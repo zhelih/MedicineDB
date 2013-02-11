2b178fda57a73772e226c2f294b97484e327a7dd
Eugene Lykhovyd <lykhovyd@gmail.com>
2013-02-11 22:32:40 +0200

    Test commit

--- src/HelloSwing.java
+++ src/HelloSwing.java
@@ -4,7 +4,7 @@ public class HelloSwing {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("HelloSwing");
-		final JLabel label = new JLabel("Hello Swing");
+		final JLabel label = new JLabel("Hello Swing bla");
 		frame.getContentPane().add(label); //different from AWT
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack(); //make compact size
